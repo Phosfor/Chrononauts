04544fcf94494011cb49791521924b673f088d51
Michael Heinrichs <michael.spamacc@googlemail.com>
2012-02-01 22:43:57 +0100

    Initial Project-setup

---
+++ Chrononauts/src/chrononauts/Chrononauts.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chrononauts;
+
+/**
+ *
+ * @author michael
+ */
+public class Chrononauts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
